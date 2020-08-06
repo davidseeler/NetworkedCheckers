e11aa4fdd8edd7c3a640ca6c51b55f70f0c7bb2d
seeler24 <das09474@uga.edu>
2020-08-06 13:34:16 -0400

    Client directories setup

---
+++ client/src/index.js
@@ -0,0 +1,28 @@
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  connection.send('hi');
+});
+
+const connection = new WebSocket('ws://127.0.0.1:4444');
+
+connection.onopen = function () {
+    console.log('Connected!');
+    connection.send('Ping'); // Send the message 'Ping' to the server
+};
+
+// Log errors
+connection.onerror = function (error) {
+    console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+connection.onmessage = function (e) {
+    console.log('Server: ' + e.data);
+};
