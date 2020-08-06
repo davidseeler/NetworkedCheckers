59a0bb55696d888a68d85d5f65841806a4b4771e
seeler24 <das09474@uga.edu>
2020-08-06 13:06:09 -0400

    Maven environment setup and Server implemented.

---
+++ src/main/java/NetworkedCheckers/Server.java
@@ -0,0 +1,54 @@
+package NetworkedCheckers;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Server extends WebSocketServer{
+
+    private static int TCP_PORT = 4444;
+    private Set<WebSocket> conns;
+
+    public Server() {
+        super(new InetSocketAddress(TCP_PORT));
+        conns = new HashSet<>();
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        conns.add(conn);
+        System.out.println("New connection from " + conn.getRemoteSocketAddress().getAddress().getHostAddress());
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        conns.remove(conn);
+        System.out.println("Closed connection to " + conn.getRemoteSocketAddress().getAddress().getHostAddress());
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        System.out.println("Message from client: " + message);
+        for (WebSocket sock : conns) {
+            sock.send(message);
+        }
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        //ex.printStackTrace();
+        if (conn != null) {
+            conns.remove(conn);
+            // do some thing if required
+        }
+        System.out.println("ERROR from " + conn.getRemoteSocketAddress().getAddress().getHostAddress());
+    }
+    public static void main(String[] args){
+        Server server = new Server();
+        server.start();
+    }
+}
